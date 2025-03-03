06889b5e571d9deb89ca4c5e8aa2901d9641aec2
Rares Dragus <rares.dragus@storyblok.com>
2025-03-03 10:07:18 +0100

    Changes from credentials[:secret_key_base] to application.secret_key_base

--- lib/active_elastic_job/rack/sqs_message_consumer.rb
+++ lib/active_elastic_job/rack/sqs_message_consumer.rb
@@ -13,11 +13,11 @@ module ActiveElasticJob
     # (2) the processed SQS message was queued by this gem representing an active job.
     # In this case it verifies the digest which is sent along with a legit SQS
     # message, and passed as an HTTP header in the resulting request.
-    # The digest is based on Rails' +credentials.secret_key_base+.
+    # The digest is based on Rails' +application.secret_key_base+.
     # Therefore, the application running in the web environment, which generates
     # the digest, and the application running in the worker
     # environment, which verifies the digest, have to use the *same*
-    # +credentials.secret_key_base+ setting.
+    # +application.secret_key_base+ setting.
     class SqsMessageConsumer
       OK_RESPONSE = [ '200'.freeze, { 'Content-Type'.freeze => 'text/plain'.freeze }, [ 'OK'.freeze ] ]
       FORBIDDEN_RESPONSE = [
